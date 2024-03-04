25889f7373df9c7c24b0ea940274086793ac9e56
mcenny <dev.mcenny@gmail.com>
2024-03-04 17:28:03 +0100

    fixed login response

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -4,6 +4,8 @@ import { UserService } from 'src/user/user.service';
 import { compare } from 'bcrypt';
 import { JwtService } from '@nestjs/jwt';
 
+const EXPIRE_TIME = 20 * 1000;
+
 @Injectable()
 export class AuthService {
   constructor(
@@ -23,16 +25,15 @@ export class AuthService {
       user,
       backendToken: {
         accessToken: await this.jwtService.signAsync(payload, {
-          expiresIn: '1h',
+          expiresIn: '1d',
           secret: process.env.jwtSecretKey,
         }),
-      },
-      refreshToken: {
         refreshToken: await this.jwtService.signAsync(payload, {
           expiresIn: '7d',
           secret: process.env.jwtRefreshTokenKey,
         }),
       },
+      expiresIn: new Date().setTime(new Date().getTime() + EXPIRE_TIME),
     };
   }
 
@@ -55,13 +56,14 @@ export class AuthService {
     };
     return {
       accessToken: await this.jwtService.signAsync(payload, {
-        expiresIn: '1h',
+        expiresIn: '20s',
         secret: process.env.jwtSecretKey,
       }),
       refreshToken: await this.jwtService.signAsync(payload, {
         expiresIn: '7d',
         secret: process.env.jwtRefreshTokenKey,
       }),
+      expiresIn: new Date().setTime(new Date().getTime() + EXPIRE_TIME),
     };
   }
 }
